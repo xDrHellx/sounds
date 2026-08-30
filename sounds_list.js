a18c66d07c34a4b9330e4b547f9104a9be437d92
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2026-08-30 23:19:49 +0200

    Added new sounds
    - RE: Outbreak
    - Initial D
    - Boktai

--- sounds_list.js
+++ sounds_list.js
@@ -1,7 +1,20 @@
 const sounds = [
-    ["RE: Outbreak 1&2", 
-        ['test', 'Jim', 'Shuddup!']
-    ] 
+    ['Resident Evil: Outbreak', 
+        ['brick', 'George', 'You have the IQ of a brick'],
+        ['dreadful', 'George', 'This is dreadful'],
+        ['shuddup', 'Jim', ''],
+        ['jesus', 'David', ''],
+        ['nice', 'David', '']
+    ],
+    ['Initial D',
+        ['initial d', '', '*commercial break*'],
+        ['initial d2', '', '*commercial break*']
+    ],
+    ['Boktai',
+        ['otenko', 'Otenko', 'Oooootenko'],
+        ['overheat', '', ''],
+        ['taiyoh', 'Django', 'Taiyoooooooh!']
+    ]
 ];
 
 export default sounds;
