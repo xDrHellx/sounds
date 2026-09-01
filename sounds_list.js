4abd7158f56b3e5967d1b2ece1b53df87374e7ed
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2026-09-01 23:36:24 +0200

    Added new sounds

--- sounds_list.js
+++ sounds_list.js
@@ -1,19 +1,52 @@
 const sounds = [
-    ['Resident Evil: Outbreak', 
-        ['brick', 'George', 'You have the IQ of a brick'],
-        ['dreadful', 'George', 'This is dreadful'],
-        ['shuddup', 'Jim', ''],
-        ['jesus', 'David', ''],
-        ['nice', 'David', '']
-    ],
-    ['Initial D',
-        ['initial d', '', '*commercial break*'],
-        ['initial d2', '', '*commercial break*']
-    ],
     ['Boktai',
         ['otenko', 'Otenko', 'Oooootenko'],
         ['overheat', '', ''],
         ['taiyoh', 'Django', 'Taiyoooooooh!']
+    ],
+    ['Initial D',
+        ['initial d', '', '*commercial break*'],
+        ['initial d2', '', '*commercial break*'],
+        ['coolvibrations', '', 'Cool vibrations']
+    ],
+    ['Resident Evil 3: Nemesis', 
+        ['barbecued', 'Carlos', 'Are you crazy?! You could have barbecued both of us!'],
+        ['couldntsave', 'Mikhail', 'I could not... save... my men!'],
+        ['notgoing', 'Dario', "NO! I'm not going anywhere!"],
+        ['notleaving', 'Dario', "I told you! I'm not leaving! NEVER!"],
+        ['ptsd', 'Mikhail', "They're coming! Get ready!"],
+        ['ptsd2', 'Mikhail', 'Fire..! Fire! Stick together!']
+    ],
+    ['Resident Evil',
+        ['monsters', 'Rebecca', 'LOOK AT THOSE MONSTEEEERRS!!'],
+        ['ouch', 'Richard', ''],
+        ['rebecca', 'Chris', 'Rebeccaaaaa!!']
+    ],
+    ['Resident Evil: Outbreak', 
+        ['heythere', 'Cindy', 'Hey there'],
+        ['poo', 'Cindy', 'Oh poo!'],
+        ['brick', 'George', 'You have the IQ of a brick'],
+        ['dreadful', 'George', 'This is dreadful'],
+        ['deer', 'George', 'Dear god!'],
+        ['hold', 'George', 'Hold your horses'],
+        ['awkward', 'George', 'This, is awkward'],
+        ['exwife', 'George', "You're worse than my ex-wife"],
+        ['nocomment', 'George', 'No comment'],
+        ['killme', 'George', 'Please KILL ME'],
+        ['shuddup', 'Jim', 'Shuddup!'],
+        ['helpme', 'Jim', 'Help me!'],
+        ['mylife', 'Jim', 'My life is shit'],
+        ['ohshit', 'Jim', 'OH SHIT HELP!'],
+        ['jesus', 'David', ''],
+        ['nice', 'David', ''],
+        ['pos', 'David', 'Piece of shit'],
+        ['thisbs', 'David', 'This is bullshit!'],
+        ['dontask', 'Alyssa', "Don't ask"],
+        ['noshit', 'Alyssa', 'No shit'],
+        ['pass', 'Alyssa', "I'll pass"],
+        ['gotiger', 'Kevin', "Go get'em tiger!"],
+        ['screwed', 'Kevin', "I'm SCREWED!"],
+        ['ualive', 'Mark', "Hot damn, you're alive!"]
     ]
 ];
 
